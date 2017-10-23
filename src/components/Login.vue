9caa693cf38c347f84776e2ba345f0b522710e7e
Gabriele Pozzan <gabriele.pozzan@cybrain.it>
2017-10-23 14:00:13 +0200

    Add very basic ajax communications with server

---
+++ src/components/Login.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="fullscreen">
+    <q-input v-model="username" float-label="Nome utente" />
+    <q-input v-model="password" type="password" float-label="Password" />
+
+    <q-btn @click="login">Invia</q-btn>
+  </div>
+</template>
+
+<script>
+import { QInput, QBtn } from 'quasar'
+
+export default {
+  components: {
+    QInput,
+    QBtn
+  },
+  data () {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    login () {
+      const options = {
+        username: this.username,
+        password: this.password
+      }
+      this.$http.post(this.config.routes.login, options)
+        .then(() => {
+          alert('ok')
+        })
+        .catch((error) => {
+          alert(error)
+        })
+    }
+  }
+}
+</script>
+
+<style>
+</style>
+
--- src/main.js
+++ src/main.js
@@ -13,7 +13,10 @@ require(`quasar/dist/quasar.${__THEME}.css`)
 import Vue from 'vue'
 import Quasar from 'quasar'
 import router from './router'
+import axios from 'axios'
 
+Vue.prototype.config = require('./config')
+Vue.prototype.$http = axios // Enable ajax requests
 Vue.config.productionTip = false
 Vue.use(Quasar) // Install Quasar Framework
 
